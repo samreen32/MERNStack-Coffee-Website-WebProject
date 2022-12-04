f4f78b5bb6887d53221ec149a72e6a7f7023720d
samreen karim <iamsamreenk@gmail.com>
2022-12-04 18:17:04 +0500

    Coffee Website-NavbarComplete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import home from "./assets/images/home-img.jpeg";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const myStyle = {
+    backgroundImage: `url(${home})`,
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={myStyle}>
+    <Navbar style={{zIndex: 1}}/>
+      <p>
+        Loremips What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the
+        1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum. Why do we use it? It is a
+        long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like).
+      </p>
+      <p>
+        Loremips What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the
+        1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum. Why do we use it? It is a
+        long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like).
+      </p>
+      <p>
+        Loremips What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the
+        1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum. Why do we use it? It is a
+        long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like).
+      </p>
+      <p>
+        Loremips What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the
+        1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum. Why do we use it? It is a
+        long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like).
+      </p>
+      <p>
+        Loremips What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has
+        survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the
+        1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum. Why do we use it? It is a
+        long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like).
+      </p>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "../App.css";
+import logo from "../assets/images/heart.png";
+
+export default function Navbar() {
+    const SearchForm =()=>{
+        let searchForm = document.querySelector('.search-form');
+        document.querySelector('#search-btn').onclick = () =>{
+            searchForm.classList.toggle('active');
+        }
+    }
+ 
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={logo}
+            alt="Logo"
+            width="9%"
+            height="9%"
+            className="d-inline-block align-text-top"
+          />
+          <p className="d-inline-block align-text-top mx-3 my-2">
+            <i>Bubble Bee Coffeèin</i>{" "}
+          </p>
+        </a>
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-4">
+              <li className="nav-item mx-1">
+                <a className="nav-link active" aria-current="page" href="#">
+                <div className="navbar-listItems">      
+                    <i className="fa-solid fa-mug-saucer mx-2"></i>
+                    <i>Home</i> 
+                </div>
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#about">
+                
+                  <i className="fa-solid fa-eject mx-2"></i>
+                  <i>About</i>
+               
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#">
+                  <i className="fa-solid fa-bars mx-2"></i>
+                  <i>Menu</i>
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#">
+                  <i className="fa-solid fa-bell-concierge mx-2"></i>
+                  <i>Service</i>
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#">
+                  <i className="fa-solid fa-comment mx-2"></i>
+                  <i>Review</i>
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#">
+                  <i className="fa-solid fa-address-card mx-2"></i>
+                  <i>Contact</i>
+                </a>
+              </li>
+              
+                <li className="nav-item mx-1">
+                    <a className="nav-link" href="#">
+                        <div id="search-btn">
+                            <i className="fa-solid fa-cart-shopping fa-xl mx-5 my-4" onClick={SearchForm}></i>
+                        </div>
+                    </a>
+                </li>
+                <div className="search-form">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                </div>
+
+              <li className="nav-item mx-1">
+                <a className="nav-link" href="#">
+                  <i className="fa-solid fa-magnifying-glass fa-xl mx-1 my-4" id="search-btn"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
